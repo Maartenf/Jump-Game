e03f8754303fae972c47f04b4dc07c49c0c247db
Maartenf <maartenforrier@hotmail.com>
2015-08-13 18:26:49 +0200

    Load images

--- JS/main.js
+++ JS/main.js
@@ -3,7 +3,16 @@
 	Main file to start the game
 */
 
+//JSON image data
+var data = '{"1":"IMG_0215.JPG"}';
+
 //Canvas can only draw when the window is loaded
 window.onload = function() {
-    Engine.init();
+
+	//Preload images and start game when loaded
+	ImageHandler.preload("Images/", data, function() {
+		//Start game engine
+    	Engine.init();
+	});
+	
 }
