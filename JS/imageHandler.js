0d5aa8fb1565809596f477ebb3af5eeb49006996
Maartenf <maartenforrier@hotmail.com>
2015-08-13 21:13:33 +0200

    Atlass loader now

--- JS/imageHandler.js
+++ JS/imageHandler.js
@@ -1,62 +1,43 @@
 /* 	imageHandler.js
 	======================================
-	Preloads and manages images
+	Preloads and draws images from atlass
 */
 
 var ImageHandler = {
 
-	//Used to store all images with their name
-	images: {},
+	//Atlass image
+	atlass: null,
 
-	//How many images need to be loaded
-	imagesToLoad: 0,
-
-	//How many images are loaded
-	imagesLoaded: 0,
+	//JSON object with image data
+	data: null,
 
 	preload: function(path, json, fallback) {
-		var data = JSON.parse(json);
+		//Convert string to object
+		this.data = JSON.parse(json);
 
-		for (var key in data) {
-			//Add one more image to be loaded
-			this.imagesToLoad++;
+		//Create image object
+		this.atlass = new Image();
 
-			//Create new image
-			var img = new Image();
-
-			//Scope fix
-			var ih = this;
-			var fb = fallback;
-
-			//When image is loaded
-			img.onload = function() {
-				//Increment number of loaded images
-				ih.imagesLoaded++;
-
-				//When all images are loaded, call fallback
-				if (ih.imagesLoaded === ih.imagesToLoad) fb();
-			}
-
-			//Set image URL
-			img.src = path + data[key];
-
-			//Store image
-			this.images[key] = img;
+		//Call fallback when image is loaded
+		this.atlass.onload = function() {
+			fallback();
 		}
+
+		//Set image URL
+		this.atlass.src = path;
 	},
 
 	drawImage: function(x, y, name, scaleFactor) {
 		//Set image smoothing off
 		ctx.mozImageSmoothingEnabled = false;
-		ctx.webkitImageSmoothingEnabled = false;
 		ctx.msImageSmoothingEnabled = false;
 		ctx.imageSmoothingEnabled = false;
 
-		//Get image from name
-		var image = this.images[name];
+		//Retrieve data to crop image from atlass
+		var imgData = this.data[name];
 
-		//Scale and draw image to canvas
-		ctx.drawImage(image, x, y, image.width * scaleFactor, image.width * scaleFactor);
+		//Crop, scale and draw image to canvas
+		ctx.drawImage(this.atlass, imgData.x, imgData.y, imgData.width, imgData.height, x, y, imgData.width * scaleFactor, imgData.height * scaleFactor);
 	}
 
 };
