e0774bb07d0dc47cf8ac93198753ae29f93ab500
Maartenf <maartenforrier@hotmail.com>
2015-08-13 21:18:37 +0200

    Add atlass data and call

--- JS/main.js
+++ JS/main.js
@@ -3,14 +3,14 @@
 	Main file to start the game
 */
 
-//JSON image data
-var data = '{"player":"player.png"}';
+//JSON atlass data
+var data = '{"right-island":{"x":0,"y":0,"width":5,"height":5},"top-island":{"x":0,"y":5,"width":5,"height":5},"top-left-island":{"x":5,"y":0,"width":5,"height":5},"top-right-island":{"x":5,"y":5,"width":5,"height":5},"bottom-island":{"x":0,"y":10,"width":5,"height":5},"bottom-left-island":{"x":5,"y":10,"width":5,"height":5},"bottom-right-island":{"x":10,"y":0,"width":5,"height":5},"center-island":{"x":10,"y":5,"width":5,"height":5},"left-island":{"x":10,"y":10,"width":5,"height":5},"player":{"x":0,"y":15,"width":5,"height":5}}';
 
-//Canvas can only draw when the window is loaded
+//Canvas can only be created when the window is loaded
 window.onload = function() {
 
 	//Preload images and start game when loaded
-	ImageHandler.preload("Images/", data, function() {
+	ImageHandler.preload("atlass.png", data, function() {
 		//Start game engine
     	Engine.init();
 	});
