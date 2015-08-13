6a8594030142e85d5a9d0361ca5004dc4e01ada2
Maartenf <maartenforrier@hotmail.com>
2015-08-13 17:26:25 +0200

    Add file header

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,8 @@
+/* 	main.js
+	======================================
+	Main file to start the game
+*/
+
 //Canvas can only draw when the window is loaded
 window.onload = function() {
     Engine.init();
