51af9ffbfde306264aa9f3c2eb7978c550c5fe6c
Maartenf <maartenforrier@hotmail.com>
2015-08-14 14:29:49 +0200

    Off screen detection

--- JS/player.js
+++ JS/player.js
@@ -9,21 +9,28 @@ var Player = {
 	x: 100,
 	y: 100,
 
-	//Size of player
-	size: 30,
-
 	//How fast the player goes
-	speed: 2,
+	speed: 6,
+
+	//Factor of image scalation
+	scaleFactor: 5,
+
+	//Size of player
+	size: 25,
 
 	//Update x-coordinate of player
 	update: function() {
 		if (KeyHandler.keys[39]) this.x += this.speed;
 		else if (KeyHandler.keys[37]) this.x -= this.speed;
+
+		//Make sure the player can't get off screen
+		if (this.x < 0) this.x = 0;
+		else if (this.x + this.size > Engine.width) this.x = Engine.width - this.size;
 	},
 
 	//Draw updated player to the canvas
 	draw: function() {
-		ImageHandler.drawImage(this.x - this.size / 2, this.y - this.size / 2, "player", 25);
+		ImageHandler.drawImage(this.x, this.y, "player", this.scaleFactor);
 	}
 
 };
