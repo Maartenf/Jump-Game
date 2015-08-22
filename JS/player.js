69c8093d2be307c72f9d886e68045d2c350e1482
Maartenf <maartenforrier@hotmail.com>
2015-08-22 16:32:29 +0200

    Add acceleration and deceleration

--- JS/player.js
+++ JS/player.js
@@ -7,10 +7,22 @@ var Player = {
 
 	//Player coordinates
 	x: 100,
-	y: 100,
+	y: 700,
 
 	//How fast the player moves
-	speed: 6,
+	speed: 0.5,
+
+	//Speed to accelerate or deccelerate to
+	desiredSpeed: 1,
+
+	//Time to change speed
+	speedChangeTime: 10,
+
+	//Minimum speed of player
+	minSpeed: 1,
+
+	//Maximum speed of player
+	maxSpeed: 5,
 
 	//Factor of image scalation
 	scaleFactor: 5,
@@ -20,8 +32,21 @@ var Player = {
 
 	//Update x-coordinate of player
 	update: function() {
-		if (KeyHandler.keys[39]) this.x += this.speed;
-		else if (KeyHandler.keys[37]) this.x -= this.speed;
+		//Accelerate or deccelerate the player when needed
+		if (this.desiredSpeed !== this.speed) {
+			//Acceleration
+			if (this.desiredSpeed > this.speed) this.speed += this.desiredSpeed / this.speedChangeTime;
+			//Deceleration
+			else this.speed -= this.desiredSpeed / this.speedChangeTime;
+		}
+
+		//Accelerate when shift is pressed
+		if (KeyHandler.keys[16]) this.accelerate();
+		else this.decelerate();
+		
+		//Move player left or right according to key input
+		if (KeyHandler.keys[39]) this.x += 6;
+		else if (KeyHandler.keys[37]) this.x -= 6;
 
 		//Make sure the player can't get off screen
 		if (this.x < 0) this.x = 0;
@@ -31,6 +56,16 @@ var Player = {
 	//Draw updated player to the canvas
 	draw: function() {
 		ImageHandler.drawImage(this.x, this.y, SpriteAnimationManager.getFrame("walk"), this.scaleFactor);
+	},
+
+	//Accelerate player
+	accelerate: function() {
+		this.desiredSpeed = this.maxSpeed;
+	},
+
+	//Decelerate player
+	decelerate: function() {
+		this.desiredSpeed = this.minSpeed;
 	}
 
 };
